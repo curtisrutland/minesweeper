8ba1946081b522f72d37702238515db68ae0e2a1
Curtis Rutland <curtisrutland@gmail.com>
2020-07-06 00:39:40 -0500

    Updates

---
+++ precache-manifest.d656c6b54bbad24589c2084dadb8c14e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a543d142f5e071c7df385b31d2a8fd",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "6f6dfee7b158747470e2",
+    "url": "/minesweeper/static/css/main.2debcf5c.chunk.css"
+  },
+  {
+    "revision": "1a45f5eacb921dc11d14",
+    "url": "/minesweeper/static/js/2.adcef40c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minesweeper/static/js/2.adcef40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6dfee7b158747470e2",
+    "url": "/minesweeper/static/js/main.fcfaa9af.chunk.js"
+  },
+  {
+    "revision": "e2c7e9f46b9595c0ecda",
+    "url": "/minesweeper/static/js/runtime-main.85a42047.js"
+  },
+  {
+    "revision": "5d5e9d0ac4fe0e7cc14e6650cee660fe",
+    "url": "/minesweeper/static/media/bomb.5d5e9d0a.svg"
+  },
+  {
+    "revision": "2dbeae25a356479a49409be59dd2b737",
+    "url": "/minesweeper/static/media/flag.2dbeae25.svg"
+  }
+]);
